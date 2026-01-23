d9edbb64560810c0bc541eb7c555e5ab9d12b192
Invictus-ill <77726890+Invictus-ill@users.noreply.github.com>
2026-01-23 21:00:04 +0530

    Refactor UI components for better structure and styling in main and expense item widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,26 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 239, 50, 50),
+);
+
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 159, 77, 174),
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(),
+        ),
       ),
       home: Expenses(),
     ),
--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -11,23 +11,26 @@ class ExpenseItem extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
       child: Card(
-        child: Column(
-          children: [
-            Text(expense.title),
-            Row(
-              children: [
-                Text('\$${expense.amount.toStringAsFixed(2)}'),
-                Spacer(),
-                Row(
-                  children: [
-                    Icon(categoryIcons[expense.category]),
-                    const SizedBox(width: 8),
-                    Text(expense.formattedDate),
-                  ],
-                ),
-              ],
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(expense.title),
+              Row(
+                children: [
+                  Text('\$${expense.amount.toStringAsFixed(2)}'),
+                  Spacer(),
+                  Row(
+                    children: [
+                      Icon(categoryIcons[expense.category]),
+                      const SizedBox(width: 8),
+                      Text(expense.formattedDate),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
